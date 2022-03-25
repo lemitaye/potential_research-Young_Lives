eb0562905f2d77f315145934c1fef49b4f2f945b
Lemi Daba <tayelemi@gmail.com>
2022-03-25 16:11:03 +0000

    created a master file for the project

---
+++ scripts/master.R
@@ -0,0 +1,28 @@
+########################################################################~
+# 
+# Project Title: " ..."
+# 
+# Project Owners: Teferi Mergo and Lemi Daba
+# R Script "master"
+# Created on: March 25, 2022
+########################################################################~
+
+# Some preparations: ####
+
+rm(list = ls())
+
+# packages
+rm(list = ls())
+
+library(tidyverse)
+library(haven)
+library(scales)
+
+theme_set(theme_light())
+
+
+# Call R scripts ####
+source("scripts/extract_clean.R", echo=TRUE, max=1000) # Data import and cleaning
+# source("scripts/descriptives.R",echo=TRUE,max=1000) # Descriptive statistics
+# source("scripts/estimation.R"  ,echo=TRUE,max=1000) # Estimation of model 
+# source("scripts/results.R"     ,echo=TRUE,max=1000) # Tables and Figures
