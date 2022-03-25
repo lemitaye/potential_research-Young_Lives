d9c3d55bf28bb0512c6cecaa72f6232c06fb9178
Lemi Daba <tayelemi@gmail.com>
2022-03-25 15:59:30 +0000

    created data extraction and cleaning script for serious work

---
+++ scripts/extract_clean.R
@@ -0,0 +1,8 @@
+
+# Date created: March 25, 2022
+# 
+# Description: This script loads the necessary data from several rounds of the
+#   Young Lives Datasets, selects relevant variables, cleans, merges, constructs 
+#   new variables (both outcomes and covariates) and
+#   produces file(s) ready for analyss.
+
